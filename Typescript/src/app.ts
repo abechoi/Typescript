a766b79b5d78679207aeeb5c8e06703468f62213
Abraham Choi <abraham.choi@icloud.com>
2020-10-31 20:17:14 -0700

    Interfaces with Classes

--- Typescript/src/app.ts
+++ Typescript/src/app.ts
@@ -24,7 +24,21 @@ const greetPerson = (person: IsPerson) => {
 }
 greetPerson(me);
 
-import {Invoice} from "./classes/Invoice.js";
+import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
+
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
+
+docOne = new Invoice("yoshi", "web work", 250);
+docTwo = new Payment("mario", "plumbing work", 200);
+
+let docs: HasFormatter[] = [];
+docs.push(docOne);
+docs.push(docTwo);
+
+console.log(docs);
 
 // DOM
 // Must add ! else it may think anchor is HTMLAnchorElement or NULL.
@@ -57,16 +71,16 @@ const anchor = document.querySelector('a')!;
 //   }
 // }
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+// const invOne = new Invoice('mario', 'work on the mario website', 250);
+// const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
 
-invoices.forEach(inv => {
-  console.log(inv.client, inv.amount, inv.format());
-});
+// invoices.forEach(inv => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
@@ -78,10 +92,13 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
-  console.log(
-    type.value,
-    tofrom.value,
-    details.value,
-    amount.valueAsNumber
-  );
+
+  let doc: HasFormatter;
+  if(type.value === "invoice"){
+    doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+  }else{
+    doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+  }
+
+  console.log(doc);
 })
